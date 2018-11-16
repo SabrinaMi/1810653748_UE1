5812bf907703dacba79526de890d236ece224c15
Sabrina Mitternoeckler <1810653748@stud.fh-kufstein.ac.at>
2018-11-16 15:01:28 +0100

    Aufgabe4

---
+++ src/Aufgabe4_1810653748.java
@@ -0,0 +1,18 @@
+public class Aufgabe4_1810653748
+{
+    public static void main (String []args)
+    {
+        int zahl1= 10;
+        int zahl2= 15;
+        System.out.println(zahl1<zahl2);
+        System.out.println(zahl2<zahl1);
+        System.out.println(zahl1>zahl2);
+        System.out.println(zahl2>zahl1);
+        System.out.println(zahl1<=zahl2);
+        System.out.println(zahl2<=zahl1);
+        System.out.println(zahl1>=zahl2);
+        System.out.println(zahl2>=zahl2);
+        System.out.println(zahl1==zahl2);
+        System.out.println(zahl1!=zahl2);
+    }
+}
